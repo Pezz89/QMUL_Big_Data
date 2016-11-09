b4d9669448036be4b9bbf49d3b22e11cd147b99e
Sam Perry <u1265119@unimail.hud.ac.uk>
2016-11-09 21:54:10 +0000

    Created MapReduce for counting tweet lengths

--- sync_to_QMUL.sh
+++ sync_to_QMUL.sh
@@ -1,11 +1,10 @@
 #!/usr/bin/env bash
 
 echo "Attempting to sync via SSHFS"
-if  rsync -Pavzur ./ ~/bert/Big_Data/Assignment_1/ --delete && rsync -Pavzur ~/bert/Big_Data/Assignment_1/ ./ ; then
+if  rsync -Pavzur ./ ~/bert/Big_Data/Assignment_1/ --exclude './build' --exclude './.git' --delete && rsync -Pavzur ~/bert/Big_Data/Assignment_1/ ./ --exclude './build' --exclude './.git' ; then
     echo "Synced via SSHFS"
 else
     echo "QMUL SSHFS not mounted, syncing via ssh..."
-    rsync -Pavzur ./
-    sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/ --delete
-    rsync -Pavzur sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/ ./  
+    rsync -Pavzur ./ sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/ --delete --exclude './build' --exclude './.git'
+    rsync -Pavzur sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/ ./ --exclude './build' --exclude './.git'
 fi 
