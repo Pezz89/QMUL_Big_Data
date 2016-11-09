d04d50e37f54ca4c3fdfe1a3e212d4c70b1c0888
Sam Perry <u1265119@unimail.hud.ac.uk>
2016-11-09 18:02:56 +0000

    Updated sync script

--- sync_to_QMUL.sh
+++ sync_to_QMUL.sh
@@ -1,10 +1,11 @@
 #!/usr/bin/env bash
 
 echo "Attempting to sync via SSHFS"
-if  rsync -Pavzur ./ ~/bert/Big_Data/Assignment_1/ && rsync -Pavzur ~/bert/Big_Data/Assignment_1/ ./ ; then
+if  rsync -Pavzur ./ ~/bert/Big_Data/Assignment_1/ --delete && rsync -Pavzur ~/bert/Big_Data/Assignment_1/ ./ ; then
     echo "Synced via SSHFS"
 else
     echo "QMUL SSHFS not mounted, syncing via ssh..."
-    rsync -Pavzur ./ sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/
+    rsync -Pavzur ./
+    sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/ --delete
     rsync -Pavzur sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/ ./  
 fi 
