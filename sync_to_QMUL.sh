d13385f948bafe6fe0e6f95af94acd385286c3fb
Sam Perry <u1265119@unimail.hud.ac.uk>
2016-11-09 17:53:03 +0000

    Added initial files

---
+++ sync_to_QMUL.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+
+echo "Attempting to sync via SSHFS"
+if  rsync -Pavzur ./ ~/bert/Big_Data/Assignment_1/ && rsync -Pavzur ~/bert/Big_Data/Assignment_1/ ./ ; then
+    echo "Synced via SSHFS"
+else
+    echo "QMUL SSHFS not mounted, syncing via ssh..."
+    rsync -Pavzur ./ sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/
+    rsync -Pavzur sp319@bert.student.eecs.qmul.ac.uk:/homes/sp319/Big_Data/Assignment_1/ ./  
+fi 
